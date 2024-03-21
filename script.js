d5b743f9e4fb7b62ea801a55e445164bdef23051
benjyalper <benjyalper@hotmail.com>
2024-03-21 17:42:32 +0200

    initial

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 $('h1').on('click', function () {
-    $('.container').addClass('animate__flipOutY')
-})
+    $('.container').addClass('animate__flipOutY');
+    setTimeout(function () {
+        $('.container').removeClass('animate__flipOutY');
+        $('.container').css('background-color', "blue");
+        $('.container').css('background', "url('./clown-cute.gif') no-repeat center");
+    }, 1000); // Adjust the delay time (in milliseconds) as needed
+});
