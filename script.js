0dae85e4ac1829312276c0c2f1bebddb357c2e7e
benjyalper <benjyalper@hotmail.com>
2024-03-21 18:06:36 +0200

    initial2

--- script.js
+++ script.js
@@ -6,4 +6,9 @@ $('button').on('click', function () {
         $('.container').css('background', "url('./clown-cute.gif') no-repeat center").addClass('animate__animated animate__bounceInRight');
         $('h1').css('visibility', 'visible');
     }, 1000); // Adjust the delay time (in milliseconds) as needed
+
+    setTimeout(function () {
+        // Redirect to a new location when the button is clicked again
+        location.href = "https://benjyalper.github.io/grosman/"; // Replace "https://example.com" with your desired URL
+    }, 2000); // Adjust the delay time (in milliseconds) as needed
 });
