08f1bd4daefdf7542b1cb1228c5b47e7e213a554
benjyalper <benjyalper@hotmail.com>
2024-03-22 15:29:20 +0200

    hello

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 $('button').on('click', function () {
+    // Flip out the initial image
     $('.container').addClass('animate__flipOutY');
+
     setTimeout(function () {
+        // Remove the flipOutY animation class
         $('.container').removeClass('animate__flipOutY');
-        $('.container').css('background', 'none'); // Set background to none to hide it
+        // Hide the initial image (set background to none)
+        $('.container').css('background', 'none');
+
+        // Set the background to the clown image and add animation classes for clown entry
+        $('.container').css('background', "url('./clown-cute.gif') no-repeat");
         $('.container').addClass('animate__animated animate__bounceInRight');
+
+        // Make the h1 and h2 elements visible
         $('h1').css('visibility', 'visible');
         $('h2').css('visibility', 'visible');
-
     }, 1000); // Adjust the delay time (in milliseconds) as needed
 
     setTimeout(function () {
