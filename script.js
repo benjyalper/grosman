bb62937a81eb4970fe552bcb64252c7d664ef866
benjyalper <benjyalper@hotmail.com>
2024-03-22 13:14:24 +0200

    hello

--- script.js
+++ script.js
@@ -5,11 +5,11 @@ $('button').on('click', function () {
         $('.container').css('background', 'none'); // Set background to none to hide it
         $('.container').addClass('animate__animated animate__bounceInRight');
         $('h1').css('visibility', 'visible');
-        $('h1').append('<h2>אוהבים מאד, משפחת אלפר!</h2>');
+        // $('h1').append('<h2>אוהבים מאד, משפחת אלפר!</h2>');
     }, 1000); // Adjust the delay time (in milliseconds) as needed
 
     setTimeout(function () {
         // Redirect to a new location when the button is clicked again
         location.href = "https://benjyalper.github.io/grosman/"; // Replace "https://example.com" with your desired URL
-    }, 2000); // Adjust the delay time (in milliseconds) as needed
+    }, 5000); // Adjust the delay time (in milliseconds) as needed
 });
